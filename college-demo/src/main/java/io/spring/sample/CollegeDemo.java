c48c46b74105f86e94005c24ff3f671b5534d77e
Prathyusha Reddy <Prathyusha Reddy@DESKTOP-G1BI2ES>
2018-01-15 11:22:44 -0600

    initial commit

---
+++ college-demo/src/main/java/io/spring/sample/CollegeDemo.java
@@ -0,0 +1,11 @@
+package io.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeDemo {
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeDemo.class, args);
+}
+	}
---
+++ college-demo/src/main/java/io/spring/sample/branch/BranchController.java
@@ -0,0 +1,26 @@
+package io.spring.sample.branch;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BranchController {
+
+	@Autowired
+	private BranchService branchservice;
+	
+	@RequestMapping("/branches")
+	public List<Branch>  getAllBranches(){
+		return branchservice.getAllBranches();
+	}
+	
+	@RequestMapping("/branches/{id}")
+	public Branch getBranch(@PathVariable String id){
+		return branchservice.getBranch(id);
+	}
+	
+}
